0605492030d2ea9fc7d466e334fa248ac718ca65
Zach Sents <zachsents@gmail.com>
2022-06-02 15:38:22 -0600

    Converted shared folder to independent module

---
+++ shared/index.js
@@ -0,0 +1,6 @@
+import db from './mongo.js'
+import Status from './status.js'
+import chartReadout from './readout_chart.js'
+import tableReadout from './readout_table.js'
+
+export { db, Status, chartReadout, tableReadout }
